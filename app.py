473dd948f5e7fb651af9f66630aec4a925ca0db8
Cookie <398212699@qq.com>
2021-02-14 20:11:47 +0800

    one last last last last time

--- app.py
+++ app.py
@@ -6,4 +6,12 @@ app=Flask(__name__)
 def print_hello():
     return "Hello From Cookie~!~!"
 
+@app.route("/pick-up-lines")
+def pick_up_lines():
+    return """
+        I love you once,
+        I love you twice,
+        I love you more than beans and rice.
+    """
+
 app.run(host='0.0.0.0',port=5000)

1bdf2f9c9901005a86854ad3e7163b719f0f1162
Cookie <398212699@qq.com>
2021-02-14 20:49:52 +0800

    one more time

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def pick_up_lines():
     return """
         I love you once,\n
         I love you twice,\n
-        I love you more than beans and rice.\n
+        I love you more than beans and rice~\n
     """
 
 app.run(host='0.0.0.0',port=5000)
